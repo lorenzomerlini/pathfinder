87fccc6e9357d2db6863be6a184e92871d4c7c80
Merlini <163999756+lorenzomerlini@users.noreply.github.com>
2024-07-22 16:27:28 +0200

    add code comments

--- execute.go
+++ execute.go
@@ -3,21 +3,21 @@ package main
 import (
 	"sync"
 )
-
+// execute is used to implement goroutine calls - SearchFile will do the proper file / extension search
 func execute(dirs []string, query string, results chan<- string, maxGoroutines int, searchType string) {
-	wg := new(sync.WaitGroup)
-	semaphore := make(chan struct{}, maxGoroutines)
+	wg := new(sync.WaitGroup)			// define a waitgroup for goroutines 
+	semaphore := make(chan struct{}, maxGoroutines)	// use a semaphore to coordinate goroutines 
 
 	for _, dir := range dirs {
-		wg.Add(1)
+		wg.Add(1)			// waitgroup + 1
 		semaphore <- struct{}{}
 		go func(dir string) {
 			defer wg.Done()
-			SearchFile(dir, query, results, searchType)
+			SearchFile(dir, query, results, searchType)	// function call SearchFile (call a goroutine)
 			<-semaphore
 		}(dir)
 	}
 
-	wg.Wait()
-	close(results)
+	wg.Wait()			// wait for all the goroutines 
+	close(results)			// close results channel
 }
