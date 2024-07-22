97420d431656561af2f6a6424d65606f1186ef9d
Merlini <163999756+lorenzomerlini@users.noreply.github.com>
2024-07-22 16:24:23 +0200

    Add code comments

--- directories.go
+++ directories.go
@@ -13,7 +13,7 @@ func getDirectories(homeDir string) []string {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
+	// iterate slice, if one element is a directory (dir.IsDir()), append to directories (slice)  (homeDir + name of directory = complete path)
 	for _, dir := range files {
 		if dir.IsDir() {
 			directories = append(directories, filepath.Join(homeDir, dir.Name()))
