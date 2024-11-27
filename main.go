69770fcb692cc2466b4ac3683a3187d3e1cb7430
Merlini <163999756+lorenzomerlini@users.noreply.github.com>
2024-11-27 10:15:29 +0100

    Resolved some bugs

--- main.go
+++ main.go
@@ -16,65 +16,84 @@ func main() {
 
 	for {
 		var query string
-		// Use of /common-nighthawk/go-figure package to create PathFinder Tool logo
+
+		// print title
 		myFigure := figure.NewColorFigure("PathFinder Tool", "linux", "red", true)
 		myFigure.Print()
+
+		// select type of search
 		fmt.Println("Select search type:")
 		fmt.Println("1. Search for a specific file")
 		fmt.Println("2. Search for all files with a specific extension")
 		searchType, _ := reader.ReadString('\n')
 		searchType = strings.TrimSpace(searchType)
 
+		// switch type of search
 		switch searchType {
 		case "1":
 			searchType = "file"
 			fmt.Println("Insert file name: ")
-			fmt.Scan(&query)
+			query, _ = reader.ReadString('\n')
+			query = strings.TrimSpace(query)
+
 		case "2":
 			searchType = "extension"
-			fmt.Println("Insert file extension: ")
-			fmt.Scan(&query)
+			fmt.Println("Insert file extension (e.g., .txt): ")
+			query, _ = reader.ReadString('\n')
+			query = strings.TrimSpace(query)
+
 		default:
 			fmt.Println("Invalid selection. Defaulting to file search.")
 			searchType = "file"
 			fmt.Println("Insert file name: ")
-			fmt.Scan(&query)
+			query, _ = reader.ReadString('\n')
+			query = strings.TrimSpace(query)
 		}
-
-		fmt.Println("Select search directory:")
-		fmt.Println("1. Home Directory")
-		fmt.Println("2. Root Directory")
-		selection, _ := reader.ReadString('\n')
-		selection = selection[:len(selection)-1] // remove newline char
 
 		var startDir string
-		switch selection {
-		case "1":
-			homeDir, _ := os.UserHomeDir()		// retrieve home directory
-			startDir = homeDir
-		case "2":
-			startDir = "/"
-		default:
-			fmt.Println("Invalid selection. Defaulting to Home Directory.")
-			homeDir, _ := os.UserHomeDir()
-			startDir = homeDir
+		for {
+
+			fmt.Println("Select search directory:")
+			fmt.Println("1. Home Directory")
+			fmt.Println("2. Root Directory")
+			selection, _ := reader.ReadString('\n')
+			selection = strings.TrimSpace(selection) // remove newline char
+
+			if selection == "1" || selection == "2" {
+				switch selection {
+				case "1":
+					homeDir, _ := os.UserHomeDir()
+					startDir = homeDir
+				case "2":
+					startDir = "/"
+
+				}
+				break
+			} else {
+
+				fmt.Println("Invalid selection. Please enter 1 or 2.")
+			}
 		}
 
-		var maxGoroutines int
+		fmt.Printf("Search Type: %s\n", searchType)
+		fmt.Printf("Query: %s\n", query)
+		fmt.Printf("Starting Directory: %s\n", startDir)
+
 		fmt.Println("Enter the maximum number of goroutines to use: ")
-		maxGoroutinesInput, _ := reader.ReadString('\n')
-		maxGoroutinesInput = maxGoroutinesInput[:len(maxGoroutinesInput)-1] // remove newline char
-		maxGoroutines, err := strconv.Atoi(maxGoroutinesInput)
-		if err != nil || maxGoroutines <= 0 {
+		maxGoroutinesinput, _ := reader.ReadString('\n')
+		maxGoroutinesinput = strings.TrimSpace(maxGoroutinesinput) // remove newline char
+		maxGoroutines, _ := strconv.Atoi(maxGoroutinesinput)
+		if maxGoroutines <= 0 {
+
 			fmt.Println("Invalid input. Using default value of 10 goroutines.")
 			maxGoroutines = 10
 		}
 
-		dirs := getDirectories(startDir)		// function call getDirectories - retrieves all the directories starting from homeDir
+		dirs := getDirectories(startDir)
 
 		results := make(chan string)
 		var found []string
-		
+
 		go func() {
 			for result := range results {
 				found = append(found, result)
@@ -82,9 +101,8 @@ func main() {
 		}()
 
 		startTime := time.Now()
-		execute(dirs, query, results, maxGoroutines, searchType)	// function call execute - parameters passed: starting directories, initial query,
-										// results channel, max number of goroutines and search type (file vs extension)
-		elapsedTime := time.Since(startTime)		// time calculator
+		execute(dirs, query, results, maxGoroutines, searchType)
+		elapsedTime := time.Since(startTime)
 
 		fmt.Println("Search executed successfully!")
 		fmt.Printf("\nFound %d paths\n", len(found))
@@ -100,7 +118,7 @@ func main() {
 			}
 		} else if response == "s" {
 
-			saveResults(found)			// function call saveResults 
+			saveResults(found)
 		}
 
 		fmt.Println("Would you like to do another search? (y/n): ")
@@ -110,11 +128,10 @@ func main() {
 			break
 		}
 
-		clearTerminal()					// function call clearTerminal
+		clearTerminal()
 	}
 }
 
-// saveResults is used to save the output paths in a .txt file, easier to analyze rather than standard output
 func saveResults(results []string) {
 
 	file, err := os.Create("paths")
@@ -122,7 +139,7 @@ func saveResults(results []string) {
 		fmt.Println("Error creating file: ", err)
 		return
 	}
-	defer file.Close()		// postpone closing 
+	defer file.Close()
 
 	writer := bufio.NewWriter(file)
 	for _, result := range results {
